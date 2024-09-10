ddb0ca6e384bb3297a2f46866c95bdb47f28ad9a
Muhammad Akasha <153507162+muhammad-akasha@users.noreply.github.com>
2024-09-10 16:58:50 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import SingalPro from "./components/SingalPro";
-import NoPage from "./pages/noPage";
+import NoPage from "./pages/NoPage";
 
 const route = createBrowserRouter([
   {
